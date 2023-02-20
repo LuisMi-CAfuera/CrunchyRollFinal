dcf91eb11ab0d8786c2dffd6a8a4153ef7ae8067
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-02-20 14:08:31 +0100

    5

--- app/src/main/java/com/example/crunchyrollfixed/Planes.kt
+++ app/src/main/java/com/example/crunchyrollfixed/Planes.kt
@@ -1,10 +1,14 @@
 package com.example.crunchyrollfixed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.crunchyrollfixed.databinding.ActivityPlanesBinding
 
 class Planes : AppCompatActivity() {
+    private lateinit var binding: ActivityPlanesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityPlanesBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planes)
     }
