2ceb2a122c089e315a03c2c86457205c47255b0d
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-02-20 14:12:38 +0100

    8

--- app/src/main/java/com/example/crunchyrollfixed/Planes.kt
+++ app/src/main/java/com/example/crunchyrollfixed/Planes.kt
@@ -3,13 +3,19 @@ package com.example.crunchyrollfixed
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.crunchyrollfixed.databinding.ActivityPlanesBinding
 
 class Planes : AppCompatActivity() {
-    private lateinit var binding: ActivityPlanesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityPlanesBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planes)
+
+        //cuando se le de click a la cruz se regresa a la pantalla principal sin binding
+        val cruz = findViewById<ImageView>(R.id.cruz3)
+        cruz.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
